970457bc927914ae6f91b78f1cc4fed8f8b37e4d
chanakya <you@example.com>
2026-09-04 14:25:46 +0530

    terraform

---
+++ 30-bastion/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    common_tags = {
+        Project = var.project
+        Environment = var.env
+        Terraform = "true"
+    }
+  ami_id = data.aws_ami.joindevops.id
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
