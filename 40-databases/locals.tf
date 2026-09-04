263d6419d472f27a3d35c95315fe5bfb45c4ac70
chanakya <you@example.com>
2026-09-04 14:26:18 +0530

    terraform

---
+++ 40-databases/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    common_tags = {
+        Project = var.project
+        Environment = var.env
+        Terraform = "true"
+    }
+  ami_id = data.aws_ami.joindevops.id
+  db_subnet_ids = split(",", data.aws_ssm_parameter.db_subnet_ids.value)[0]
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+}
