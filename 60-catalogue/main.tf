5ebe960d396ffb0aa8d795d2a544223b9ee9d91c
chanakya <you@example.com>
2026-09-21 11:18:14 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -21,7 +21,7 @@ resource "terraform_data" "catalogue" {
     type     = "ssh"
     user     = "ec2-user"
     password = "DevOps321"
-    host     = aws_instance.catalogue.id
+    host     = aws_instance.catalogue.private_ip
   }
 
   provisioner "file" {
