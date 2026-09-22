0d94ed81a41c7d0a162c8cd91abe117da79d0c0e
chanakya <you@example.com>
2026-09-22 13:51:44 +0530

    terraform

--- 50-alb/parameters.tf
+++ 50-alb/parameters.tf
@@ -1,12 +1,5 @@
-
-
 resource "aws_ssm_parameter" "backend_alb_listener_arn" {
-  count = length(var.sg_names)
-  name  = "/${var.project}/${var.env}/${(var.sg_names[count.index])}_backend_alb_listener__arn"
+  name  = "/${var.project}/${var.env}/backend_alb_listener_arn"
   type  = "String"
-  value = aws_lb_listener.http_listener.arn
-  overwrite = true  # ✅ This will overwrite the existing parameter
-
-
-
+  value = aws_lb_listener.http.arn
 }
