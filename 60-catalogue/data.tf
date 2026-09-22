deb86c702cf0abb1fb59ec074a99f3aa7942ef2e
chanakya <you@example.com>
2026-09-22 13:56:59 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -18,12 +18,18 @@ data "aws_ami" "joindevops" {
   }
 }
 
-
 data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.env}/catalogue_sg_id"
+}
+
+data "aws_ssm_parameter" "vpc_id" {
+    name = "/${var.project}/${var.env}/vpc_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.env}/private_subnet_ids"
 }
 
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+    name = "/${var.project}/${var.env}/backend_alb_listener_arn"
+}
