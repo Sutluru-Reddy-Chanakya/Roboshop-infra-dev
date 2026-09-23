4fb968c09581fa7d031433361863e0f876657caf
chanakya <you@example.com>
2026-09-23 18:08:35 +0530

    terraform

--- 80-frontend/main.tf
+++ 80-frontend/main.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-01"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = local.frontend_alb_certificate_arn
 
   default_action {
