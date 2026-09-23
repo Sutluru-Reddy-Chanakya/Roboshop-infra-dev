719baa87db36758cf518f3f2b3d667176989a7be
chanakya <you@example.com>
2026-09-23 17:52:04 +0530

    terraform

--- 80-frontend/main.tf
+++ 80-frontend/main.tf
@@ -36,7 +36,7 @@ resource "aws_lb_listener" "https" {
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
-  name    = "*.frontend-alb-${var.env}.${var.domain_name}"
+  name    = "*.${var.domain_name}"
   type    = "A"
   
   # load balancer details
@@ -45,4 +45,5 @@ resource "aws_route53_record" "www" {
     zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
   }
+  allow_overwrite = true
 }
